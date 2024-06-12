4f31ec40af5e5155444705af7cd5266b129e6d3b
Bekzod <bekzodtoxtamuratov@gmail.com>
2024-06-12 23:39:38 +0300

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <script setup>
-
-import Navbar from './components/Navbar.vue';
-import Carousel from './components/Carousel.vue';
-
+import Navbar from './components/Navbar.vue'
 </script>
-
 
 <template>
 	<div>
+		<h2>App vue bu hisobladna</h2>
 		<h2>App vue bu hisobladna</h2>
 		<Navbar />
 
 		<!-- <Carousel /> -->
 	</div>
 </template>
-
 
 <style lang="scss" scoped></style>
