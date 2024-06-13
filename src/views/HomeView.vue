61d6e378b13fd5cfe07a2efb00a13a6486655d81
AbdullohXakimov <Abdullohh745@gmail.com>
2024-06-13 10:20:28 +0500

    dostavka tugadi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <script setup>
-import Intro from '../components/Intro.vue'
-import Navbar from '../components/Navbar.vue'
-import Banner from '../components/Banner.vue'
-import Products from '../components/Products.vue'
-
+import Intro from "../components/Intro.vue";
+import Navbar from "../components/Navbar.vue";
+import Banner from "../components/Banner.vue";
+import Products from "../components/Products.vue";
+import Dostavka from "../components/Dostavka.vue";
 </script>
 
 <template>
-	<div>
-		<Navbar />
-		<Intro />
-		<Banner />
-        	<Products />
-	</div>
+  <div>
+    <Navbar />
+    <Intro />
+    <Banner />
+    <Products />
+    <Dostavka />
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
