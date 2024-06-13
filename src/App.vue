a1fb6de797f3d9d00b10e6451332ece147934b46
ja1423 <aziabdujabborova1423@gmail.com>
2024-06-13 08:19:40 +0500

    banner and products

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
 	<div>
 		<h2>App vue bu hisobladna</h2>
+		<Banner />
+		<router-view />
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import Banner from "./components/Banner.vue"
+</script>
 
 <style lang="scss" scoped></style>
