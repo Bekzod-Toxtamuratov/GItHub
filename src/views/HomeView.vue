e073a8378348c2b67a3d01237b1b3861d9b4ba58
ja1423 <aziabdujabborova1423@gmail.com>
2024-06-18 21:01:00 +0500

    novosti

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@ import Market from '../components/Market.vue'
 import Navbar from '../components/Navbar.vue'
 import PopularProducts from '../components/PopularProducts.vue'
 import Products from '../components/Products.vue'
-// import Novosti from '../components/Novosti.vue'
+import Novosti from '../components/Novosti.vue'
 import skidkaTovar from '../components/skidkaTovar.vue'
 import ToTop from '../components/ToTop.vue'
 </script>
@@ -21,7 +21,7 @@ import ToTop from '../components/ToTop.vue'
 		<Products />
 		<Dostavka />
 		<skidkaTovar />
-		<!-- <Novosti /> -->
+		<Novosti />
 		<Footer />
 		<ToTop />
 	</div>
