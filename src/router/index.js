f13e42e84e8ab4dddce3b37aaa700ed6e2aa6405
ja1423 <aziabdujabborova1423@gmail.com>
2024-06-18 21:01:00 +0500

    novosti

--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,47 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-
-			path: '/',
-			name: 'login',
-			component: () => import('../components/auth/Login.vue'),
-		},
-{
-			path: '/home',
-			name: 'home',
-			component: () => import('../views/HomeView.vue'),
-		},
-		{
-			path: '/tovar',
-			name: 'tovar',
-			component: () => import('../views/TovarView.vue'),
-		},
-		{
-			path: '/magazin',
-			name: 'magazin',
-			component: () => import('../views/VseMagazinView.vue'),
-
-			path: '/home/:id',
-			name: 'product-detail',
-			component: () => import('../views/TovarView.vue'),
-		},
-		{
-			path: '/about',
-			name: 'about',
-			component: () => import('../views/Onas.View.vue'),
-
-		},
-	
-	scrollBehavior(to, from, savedPosition) {
-		if (savedPosition) {
-			return savedPosition
-		} else{
-			return :{ top: 0 },
-		}
-	},
-],
-})
-
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "login",
+      component: () => import("../components/auth/Login.vue"),
+    },
+    {
+      path: "/home",
+      name: "home",
+      component: () => import("../views/HomeView.vue"),
+    },
+    {
+      path: "/tovar",
+      name: "tovar",
+      component: () => import("../views/TovarView.vue"),
+    },
+    {
+      path: "/home/:id",
+      name: "product-detail",
+      component: () => import("../views/TovarView.vue"),
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () => import("../views/Onas.View.vue"),
+    },
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
+});
 router.beforeEach((to, from) => {
-	const isLoggedin = JSON.parse(localStorage.getItem('user'))
+  const isLoggedin = JSON.parse(localStorage.getItem("user"));
 
-	if (!isLoggedin && to.name != 'login') {
-		return { name: 'login' }
-	}
-})
-export default router
+  if (!isLoggedin && to.name != "login") {
+    return { name: "login" };
+  }
+});
+export default router;
