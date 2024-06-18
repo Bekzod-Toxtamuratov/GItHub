7a671c99cef90a9dad03cd996bbb27b88f232b5d
Bekzod <bekzodtoxtamuratov@gmail.com>
2024-06-18 15:24:58 +0300

    footer tugadi

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
 			path: '/',
+			name: 'login',
+			component: () => import('../components/auth/Login.vue'),
+		},
+		{
+			path: '/home',
 			name: 'home',
 			component: () => import('../views/HomeView.vue'),
 		},
