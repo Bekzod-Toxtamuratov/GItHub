8a4fa428c5bba1ff1e7a5e0c3e76f9d6edf5aadd
Bekzod <bekzodtoxtamuratov@gmail.com>
2024-06-20 10:38:11 +0300

    12

--- src/components/ToTop.vue
+++ src/components/ToTop.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 <template>
 	<button
-		@click=""
+	
 		class="flex items-center justify-center w-10 h-10 rounded-full bg-orange-400 text-white fixed bottom-6 right-10 z-20">
 		top
 	</button>
