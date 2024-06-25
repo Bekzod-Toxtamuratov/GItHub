62e519056690d9292f991b4461974036f82de8a7
Bekzod <bekzodtoxtamuratov@gmail.com>
2024-06-25 10:45:08 +0300

    ok:

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,12 +1,14 @@
 <script setup>
-import Footer from '../components/Footer.vue'
-import Navbar from '../components/Navbar.vue'
 import Contact from '../components/Contact.vue'
+import Footer from '../components/Footer.vue'
+import Landing from '../components/Landing.vue'
+import Navbar from '../components/Navbar.vue'
 </script>
 <template>
 	<div>
 		<Navbar />
 		<Contact class="mt-20 mb-20"/>
+		<Landing/>
 		<Footer />
 		<ToTop />
 	</div>
